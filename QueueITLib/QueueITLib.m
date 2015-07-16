caeee5bea5189bbafcc7971e2397187b44390aca
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-07-16 15:02:42 +0200

    Initial Commit

---
+++ QueueITLib/QueueITLib.m
@@ -0,0 +1,13 @@
+//
+//  QueueITLib.m
+//  QueueITLib
+//
+//  Created by Alexander Yaremchuk on 7/16/15.
+//  Copyright (c) 2015 Qeueuit. All rights reserved.
+//
+
+#import "QueueITLib.h"
+
+@implementation QueueITLib
+
+@end
