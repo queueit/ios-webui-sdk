e7d95861bc0c9a71a0547ce5b666cdfd80a18b66
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-07-16 16:02:07 +0200

    added queue service

---
+++ QueueITLib/QueueService.h
@@ -0,0 +1,13 @@
+//
+//  QueueService.h
+//  QueueITLib
+//
+//  Created by Alexander Yaremchuk on 7/16/15.
+//  Copyright (c) 2015 Qeueuit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QueueService : NSObject
+
+@end
---
+++ QueueITLib/QueueService.m
@@ -0,0 +1,13 @@
+//
+//  QueueService.m
+//  QueueITLib
+//
+//  Created by Alexander Yaremchuk on 7/16/15.
+//  Copyright (c) 2015 Qeueuit. All rights reserved.
+//
+
+#import "QueueService.h"
+
+@implementation QueueService
+
+@end
