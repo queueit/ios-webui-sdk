a0d40b57beaf0d633fa3bac92059fafac9ef02cc
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2017-09-20 09:39:59 +0200

    aligning types

--- QueueITLib/QueueStatus.h
+++ QueueITLib/QueueStatus.h
@@ -6,8 +6,8 @@
 @property (nonatomic, strong)NSString* queueUrlString;
 @property (nonatomic, strong) NSString* eventTargetUrl;
 @property (nonatomic, strong) NSString* queueitToken;
-@property Boolean inPostQueueState;
 @property int queueUrlTTL;
+@property BOOL inPostQueueState;
 
 -(instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
