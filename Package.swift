d51f9f6286c05c01937d88e61e9f7c6df2639655
Alexandre Nussbaumer <alexandre.nussbaumer@qoqa.com>
2021-08-06 17:09:13 +0200

    Add Swift Package Manager compatibility

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "QueueITLib",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "QueueITLib",
+            targets: ["QueueITLib"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QueueITLib",
+            dependencies: [])
+        .testTarget(
+            name: "QueueITLibTests",
+            dependencies: ["QueueITLib"])
+    ],
+    swiftLanguageVersions: [
+        .version("5")
+    ]
+)
