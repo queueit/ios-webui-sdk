8e5e08871c3bae3fdc4c6565aa9bfdae711419ef
Queue-it devs <devs@queue-it.com>
2022-06-30 13:40:22 +0200

    cleaned up commit

--- dist/QueueITLib.xcframework/ios-arm64_x86_64-simulator/Headers/QueueConsts.h
+++
@@ -1,8 +0,0 @@
-#ifndef QueueConsts_h
-#define QueueConsts_h
-
-#define QueueCloseUrl @"queueit://close"
-#define QueueRestartSessionUrl @"queueit://restartSession"
-#define SDKVersion @"iOS-3.0.14";
-
-#endif
